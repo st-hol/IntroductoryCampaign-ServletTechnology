c3da786fc84bbe9a37e25e7af54b643b61e80f20
Stas <stanislav.holovachuk@gmail.com>
2019-04-21 00:14:21 +0300

    logging provided

--- src/main/java/org/training/controller/Servlet.java
+++ src/main/java/org/training/controller/Servlet.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-//directory "c"omand starts from cyrillic "c"... refactor it
 
 public class Servlet extends HttpServlet {
     private Map<String, Command> commands = new HashMap<>();
